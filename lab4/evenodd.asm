96e4f97e845f0708c7891f36ccdf4390b2265902
Biomedical Lab <kinjawadekaradi112@gmail.com>
2025-01-29 17:02:43 +0530

    even odd counter

---
+++ lab4/evenodd.asm
@@ -0,0 +1,25 @@
+ASSUME CODE:CS DS:DATA    
+
+DATA SEGMENT
+    LIST DB 01H,02H,03H,04H,05H
+    EVEN DB 01 DUP(0)
+    ODD DB 01 DUP(0)
+    DATA ENDS
+
+CODE SEGMENT
+    START: MOV AX,DATA
+           MOV DS,AX
+           MOV CL, 05H
+           MOV SI ,OFFSET LIST
+           L1:MOV AL,[SI]
+           RRC AL,01
+           JNC EVEN
+           INC SI
+           DEC CL 
+           JNZ L1
+           MOV AL, RESULT
+           MOV [DI], AL
+           MOV AH,4CH
+           INT 21H
+           CODE ENDS
+END START
